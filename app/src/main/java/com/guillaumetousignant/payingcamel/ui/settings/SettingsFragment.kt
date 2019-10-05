92c4810f98d8f91292793da4512b1d77e43f2c87
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-10-05 07:44:22 -0400

    Settings screen now works

--- app/src/main/java/com/guillaumetousignant/payingcamel/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/guillaumetousignant/payingcamel/ui/settings/SettingsFragment.kt
@@ -1,25 +1,25 @@
 package com.guillaumetousignant.payingcamel.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-//import androidx.preference.PreferenceFragmentCompat // Added
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat // Added
+//import androidx.lifecycle.Observer
+//import androidx.lifecycle.ViewModelProviders
 import com.guillaumetousignant.payingcamel.R
 
-class SettingsFragment : Fragment() { // Changed
+class SettingsFragment : PreferenceFragmentCompat() { // Changed
 
-    private lateinit var settingsViewModel: SettingsViewModel
+    //private lateinit var settingsViewModel: SettingsViewModel
 
-    //override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) { // Added
-    //    setPreferencesFromResource(R.xml.preference_screen, rootKey) // Added
-    //} // Added
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) { // Added
+        setPreferencesFromResource(R.xml.preference_screen, rootKey) // Added
+    } // Added
 
-    override fun onCreateView(
+    /*override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,5 +32,5 @@ class SettingsFragment : Fragment() { // Changed
             textView.text = it
         })
         return root
-    }
+    }*/
 }
