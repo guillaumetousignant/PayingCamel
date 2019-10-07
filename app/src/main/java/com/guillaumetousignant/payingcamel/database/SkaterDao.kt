06e4d9bbb942a2b454672b293ae521e99d670a86
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-10-07 19:04:52 -0400

    Added Dao objects

---
+++ app/src/main/java/com/guillaumetousignant/payingcamel/database/SkaterDao.kt
@@ -0,0 +1,28 @@
+package com.guillaumetousignant.payingcamel.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface SkaterDao {
+
+    // LiveData is a data holder class that can be observed within a given lifecycle.
+    // Always holds/caches latest version of data. Notifies its active observers when the
+    // data has changed. Since we are getting all the contents of the database,
+    // we are notified whenever any of the database contents have changed.
+    @Query("SELECT * from skater_table ORDER BY first_name ASC")
+    fun getAscFirstNameSkater(): LiveData<List<Skater>>
+
+    @Query("SELECT * from skater_table ORDER BY last_name ASC")
+    fun getAscLastNameSkater(): LiveData<List<Skater>>
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun insert(skater: Skater)
+
+
+    @Query("DELETE FROM skater_table")
+    fun deleteAll()
+}
