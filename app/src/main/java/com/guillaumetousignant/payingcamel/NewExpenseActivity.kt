7d1745099f98ed9cc4106a8c3bd29ce0e1234f47
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-10-20 09:41:54 -0400

    Added empty activities

---
+++ app/src/main/java/com/guillaumetousignant/payingcamel/NewExpenseActivity.kt
@@ -0,0 +1,12 @@
+package com.guillaumetousignant.payingcamel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewExpenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_expense)
+    }
+}
---
+++ app/src/main/java/com/guillaumetousignant/payingcamel/NewRateActivity.kt
@@ -0,0 +1,12 @@
+package com.guillaumetousignant.payingcamel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewRateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_rate)
+    }
+}
