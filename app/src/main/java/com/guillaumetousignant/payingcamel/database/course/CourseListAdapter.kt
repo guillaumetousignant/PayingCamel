7cc7a05c6578c5fe4cecdb233559d2ab0910be4b
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-10-24 20:15:21 -0400

    Added itemdetailslookup and first letter circle

--- app/src/main/java/com/guillaumetousignant/payingcamel/database/course/CourseListAdapter.kt
+++ app/src/main/java/com/guillaumetousignant/payingcamel/database/course/CourseListAdapter.kt
@@ -1,15 +1,15 @@
 package com.guillaumetousignant.payingcamel.database.course
 
-//import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.recyclerview.selection.ItemDetailsLookup
 import androidx.recyclerview.widget.RecyclerView
 import com.guillaumetousignant.payingcamel.R
-
-//import androidx.fragment.app.Fragment
-
+import java.util.*
 
 class CourseListAdapter internal constructor(
     //context: Context
@@ -23,6 +23,15 @@ class CourseListAdapter internal constructor(
 
     inner class CourseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val courseItemView: TextView = itemView.findViewById(R.id.textView)
+        val firstLetterView: TextView = itemView.findViewById(R.id.first_letter)
+        val firstLetterCircle: RelativeLayout = itemView.findViewById(R.id.first_letter_circle)
+        lateinit var uuid: String
+
+        fun getItemDetails(): ItemDetailsLookup.ItemDetails<String> =
+            object : ItemDetailsLookup.ItemDetails<String>() {
+                override fun getPosition(): Int = adapterPosition
+                override fun getSelectionKey(): String? = uuid
+            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
@@ -35,6 +44,9 @@ class CourseListAdapter internal constructor(
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
         val current = courses[position]
         holder.courseItemView.text = current.name?:"(...)"
+        holder.firstLetterView.text = current.name?.substring(0,1)?.toUpperCase(Locale.getDefault())?:"-"
+        //holder.firstLetterCircle.setBackgroundColor(Color.GREEN)
+        holder.uuid = current.uuid.toString()
 
         holder.courseItemView.setOnClickListener {
             listener(current)
@@ -47,4 +59,6 @@ class CourseListAdapter internal constructor(
     }
 
     override fun getItemCount() = courses.size
+
+    //override fun getItemId(position: Int): Long = position.toLong()
 }
