d68f4e90c1862b5c9205f8d7f498b95953f8f2fd
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-12-28 13:43:01 -0500

    Added remaining list adapters

--- app/src/main/java/com/guillaumetousignant/payingcamel/database/rate/RateListAdapter.kt
+++ app/src/main/java/com/guillaumetousignant/payingcamel/database/rate/RateListAdapter.kt
@@ -1,18 +1,21 @@
 package com.guillaumetousignant.payingcamel.database.rate
 
-//import android.content.Context
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.widget.RecyclerView
 import com.guillaumetousignant.payingcamel.R
-
-//import androidx.fragment.app.Fragment
-
+import com.guillaumetousignant.payingcamel.ui.helpers.FlipAnimator
+import java.util.Locale
 
 class RateListAdapter internal constructor(
-    //context: Context
+    val context: Context?,
     //inflater_in: LayoutInflater
     val listener: (Rate) -> Unit
 ) : RecyclerView.Adapter<RateListAdapter.RateViewHolder>() {
@@ -20,9 +23,33 @@ class RateListAdapter internal constructor(
     //private val inflater: LayoutInflater = LayoutInflater.from(context)
     //private val inflater = inflater_in
     private var rates = emptyList<Rate>() // Cached copy of words
+    var tracker: SelectionTracker<String>? = null
 
     inner class RateViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val rateItemView: TextView = itemView.findViewById(R.id.textView)
+        val iconText: TextView = itemView.findViewById(R.id.icon_text)
+        val imgProfile: ImageView = itemView.findViewById(R.id.icon_profile)
+        val iconBack: RelativeLayout = itemView.findViewById(R.id.icon_back)
+        val iconFront: RelativeLayout = itemView.findViewById(R.id.icon_front)
+        val iconContainer: RelativeLayout = itemView.findViewById(R.id.icon_container)
+        var uuid: String? = null
+
+        fun getItemDetails(): ItemDetailsLookup.ItemDetails<String> =
+            object : ItemDetailsLookup.ItemDetails<String>() {
+                override fun getPosition(): Int = adapterPosition
+                override fun getSelectionKey(): String? = uuid
+            }
+
+        fun bind(rate: Rate, position: Int, isActivated: Boolean = false) {
+            rateItemView.text = String.format(context?.getString(R.string.title_default)?:"%s", rate.name?:context?.getString(R.string.name_empty)?:"(...)")
+            itemView.isActivated = isActivated
+            iconText.text = rate.name?.substring(0,1)?.toUpperCase(Locale.getDefault())?:"-"
+            uuid = rate.uuid.toString()
+
+            applyIconAnimation(this, position, uuid)
+            applyProfilePicture(this, rate)
+            //applyClickEvents(this, position)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RateViewHolder {
@@ -34,7 +61,8 @@ class RateListAdapter internal constructor(
 
     override fun onBindViewHolder(holder: RateViewHolder, position: Int) {
         val current = rates[position]
-        holder.rateItemView.text = current.name?:"(..)"
+        val selected = tracker?.isSelected(holder.uuid)?:false
+        holder.bind(current, position, selected)
 
         holder.rateItemView.setOnClickListener {
             listener(current)
@@ -47,4 +75,57 @@ class RateListAdapter internal constructor(
     }
 
     override fun getItemCount() = rates.size
+
+    private fun applyProfilePicture(holder: RateViewHolder, rate: Rate) {
+        /*if (!TextUtils.isEmpty(message.getPicture())) {
+            Glide.with(mContext).load(message.getPicture())
+                .thumbnail(0.5f)
+                .crossFade()
+                .transform(CircleTransform(mContext))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.imgProfile)
+            holder.imgProfile.setColorFilter(null)
+            holder.iconText.setVisibility(View.GONE)
+        } else {*/
+        holder.imgProfile.setImageResource(R.drawable.circle_shape_grey)
+        holder.imgProfile.setColorFilter(rate.color)
+        holder.iconText.visibility  = View.VISIBLE
+        //}
+    }
+
+    private fun applyIconAnimation(holder: RateViewHolder, position: Int, key: String?) {
+        if (tracker?.isSelected(key) == true) {
+            holder.iconFront.visibility = View.GONE
+            resetIconYAxis(holder.iconBack)
+            holder.iconBack.visibility = View.VISIBLE
+            holder.iconBack.alpha = 1.0f
+            //if (currentSelectedIndex === position) {
+            context?.let {
+                FlipAnimator.flipView(it, holder.iconBack, holder.iconFront, true)
+            }
+            //    resetCurrentIndex()
+            //}
+        } else {
+            holder.iconBack.visibility  = View.GONE
+            resetIconYAxis(holder.iconFront)
+            holder.iconFront.visibility = View.VISIBLE
+            holder.iconFront.alpha = 1.0f
+            /*if (reverseAllAnimations && animationItemsIndex.get(
+                    position,
+                    false
+                ) || currentSelectedIndex === position
+            ) {*/
+            context?.let {
+                FlipAnimator.flipView(it, holder.iconBack, holder.iconFront, false)
+            }
+            //resetCurrentIndex()
+            //}
+        }
+    }
+
+    private fun resetIconYAxis(view: View) {
+        if (view.rotationY != 0f) {
+            view.rotationY = 0f
+        }
+    }
 }
--- app/src/main/java/com/guillaumetousignant/payingcamel/database/trip/TripListAdapter.kt
+++ app/src/main/java/com/guillaumetousignant/payingcamel/database/trip/TripListAdapter.kt
@@ -1,27 +1,55 @@
 package com.guillaumetousignant.payingcamel.database.trip
 
-//import android.content.Context
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.widget.RecyclerView
 import com.guillaumetousignant.payingcamel.R
-
-//import androidx.fragment.app.Fragment
-
+import com.guillaumetousignant.payingcamel.ui.helpers.FlipAnimator
+import java.util.Locale
 
 class TripListAdapter internal constructor(
-    //context: Context
+    val context: Context?,
     //inflater_in: LayoutInflater
+    val listener: (Trip) -> Unit
 ) : RecyclerView.Adapter<TripListAdapter.TripViewHolder>() {
 
     //private val inflater: LayoutInflater = LayoutInflater.from(context)
     //private val inflater = inflater_in
     private var trips = emptyList<Trip>() // Cached copy of words
+    var tracker: SelectionTracker<String>? = null
 
     inner class TripViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tripItemView: TextView = itemView.findViewById(R.id.textView)
+        val iconText: TextView = itemView.findViewById(R.id.icon_text)
+        val imgProfile: ImageView = itemView.findViewById(R.id.icon_profile)
+        val iconBack: RelativeLayout = itemView.findViewById(R.id.icon_back)
+        val iconFront: RelativeLayout = itemView.findViewById(R.id.icon_front)
+        val iconContainer: RelativeLayout = itemView.findViewById(R.id.icon_container)
+        var uuid: String? = null
+
+        fun getItemDetails(): ItemDetailsLookup.ItemDetails<String> =
+            object : ItemDetailsLookup.ItemDetails<String>() {
+                override fun getPosition(): Int = adapterPosition
+                override fun getSelectionKey(): String? = uuid
+            }
+
+        fun bind(trip: Trip, position: Int, isActivated: Boolean = false) {
+            tripItemView.text = String.format(context?.getString(R.string.title_default)?:"%s", trip.name?:context?.getString(R.string.name_empty)?:"(...)")
+            itemView.isActivated = isActivated
+            iconText.text = trip.name?.substring(0,1)?.toUpperCase(Locale.getDefault())?:"-"
+            uuid = trip.uuid.toString()
+
+            applyIconAnimation(this, position, uuid)
+            applyProfilePicture(this, trip)
+            //applyClickEvents(this, position)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TripViewHolder {
@@ -33,7 +61,12 @@ class TripListAdapter internal constructor(
 
     override fun onBindViewHolder(holder: TripViewHolder, position: Int) {
         val current = trips[position]
-        holder.tripItemView.text = current.name?:"(...)"
+        val selected = tracker?.isSelected(holder.uuid)?:false
+        holder.bind(current, position, selected)
+
+        holder.tripItemView.setOnClickListener {
+            listener(current)
+        }
     }
 
     internal fun setTrips(trips: List<Trip>) {
@@ -42,4 +75,57 @@ class TripListAdapter internal constructor(
     }
 
     override fun getItemCount() = trips.size
+
+    private fun applyProfilePicture(holder: TripViewHolder, trip: Trip) {
+        /*if (!TextUtils.isEmpty(message.getPicture())) {
+            Glide.with(mContext).load(message.getPicture())
+                .thumbnail(0.5f)
+                .crossFade()
+                .transform(CircleTransform(mContext))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.imgProfile)
+            holder.imgProfile.setColorFilter(null)
+            holder.iconText.setVisibility(View.GONE)
+        } else {*/
+        holder.imgProfile.setImageResource(R.drawable.circle_shape_grey)
+        holder.imgProfile.setColorFilter(trip.color)
+        holder.iconText.visibility  = View.VISIBLE
+        //}
+    }
+
+    private fun applyIconAnimation(holder: TripViewHolder, position: Int, key: String?) {
+        if (tracker?.isSelected(key) == true) {
+            holder.iconFront.visibility = View.GONE
+            resetIconYAxis(holder.iconBack)
+            holder.iconBack.visibility = View.VISIBLE
+            holder.iconBack.alpha = 1.0f
+            //if (currentSelectedIndex === position) {
+            context?.let {
+                FlipAnimator.flipView(it, holder.iconBack, holder.iconFront, true)
+            }
+            //    resetCurrentIndex()
+            //}
+        } else {
+            holder.iconBack.visibility  = View.GONE
+            resetIconYAxis(holder.iconFront)
+            holder.iconFront.visibility = View.VISIBLE
+            holder.iconFront.alpha = 1.0f
+            /*if (reverseAllAnimations && animationItemsIndex.get(
+                    position,
+                    false
+                ) || currentSelectedIndex === position
+            ) {*/
+            context?.let {
+                FlipAnimator.flipView(it, holder.iconBack, holder.iconFront, false)
+            }
+            //resetCurrentIndex()
+            //}
+        }
+    }
+
+    private fun resetIconYAxis(view: View) {
+        if (view.rotationY != 0f) {
+            view.rotationY = 0f
+        }
+    }
 }
