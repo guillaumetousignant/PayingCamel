3955a4a959a10c4081cb825fdde87d973768fb47
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-07-14 13:45:48 -0400

    Added list skater picker

---
+++ app/src/main/java/com/guillaumetousignant/payingcamel/ui/pickers/SkatersPickerFragment.kt
@@ -0,0 +1,44 @@
+package com.guillaumetousignant.payingcamel.ui.pickers
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.guillaumetousignant.payingcamel.R
+import com.guillaumetousignant.payingcamel.database.skater.Skater
+import com.guillaumetousignant.payingcamel.database.skater.SkaterListAdapter
+
+class SkatersPickerFragment(val skater: MutableLiveData<Skater?>, private val allSkaters: LiveData<List<Skater>>) : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.skater_picker_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.skater_picker_dialog_recyclerview)
+        val adapter = SkaterListAdapter(context) {
+            skater.postValue(it)
+            dismiss()
+        }
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(activity) // CHECK can return null
+
+        allSkaters.observe(this, Observer { skaters ->
+            // Update the cached copy of the words in the adapter.
+            skaters?.let { adapter.setSkaters(it) }
+        })
+    }
+}
