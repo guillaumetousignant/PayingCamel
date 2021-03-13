3f919638ebd89c95008c7c10841cff67150010c6
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-03-13 08:25:19 -0500

    Switched to FragmentContainerView, which sucks by the way.

--- app/src/main/java/com/guillaumetousignant/payingcamel/MainActivity.kt
+++ app/src/main/java/com/guillaumetousignant/payingcamel/MainActivity.kt
@@ -11,6 +11,7 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -24,7 +25,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
